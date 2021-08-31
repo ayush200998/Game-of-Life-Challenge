53a741d758e4a79d2568d4a34c2ddd69308a96e5
Ayush Khakharia <khakharia.ayush20@gmail.com>
2021-08-31 13:33:36 +0530

    changes made

--- gameOfLife.cpp
+++ gameOfLife.cpp
@@ -2,12 +2,27 @@
 #include "Board.h"
 using namespace std;
 
+int row =3 , col = 3;
+
+int countNeighbourCell (int grid[3][3], int r, int c){
+    int count =0;
+    for(int i=r-1; i<=r+1; i++){
+        for(int j=c-1; j<=c+1; j++){
+            if((i==r && j==c) || (i<0 || j<0) || (i>=row || j>=col)){
+                continue;
+            }
+            if(grid[i][j] == 1){
+                count += 1;
+            }
+        }
+    }
+    return count;
+}
+
 int main(){
-    int row, col;
-    cout<< "Enter number of rows and columns. \n";
-    cin>> row >> col;
-    int initialGrid[row][col];
-    int grid[row][col];
+    int neighbourCount =0;
+    int initialGrid[3][3];
+    int grid[3][3];
 
     // Initialize the Grid.
     for(int i=0; i<row; i++){
@@ -22,7 +37,7 @@ int main(){
             cout<< initialGrid[i][j] << " " ;
         }
         cout<<endl;
-     }
+    }
     
     cout<< "How much input you want?. \n";
     int max;
@@ -49,94 +64,21 @@ int main(){
         }
         cout<<endl;
     }
-
     // Logic to find life and death. 
     for(int i=0; i<row; i++){
-        for(int j=0; j<col;j++){
-            int count = 0;
-            // Checking for death. 8 sides to check therefore 8 conditions.
-            if(initialGrid[i][j] == 0){
-                // Adjacent Columns
-                cout<< initialGrid[i][j-1] <<endl; 
-                if(initialGrid[i][j-1] == 1 && j!=0){
-                    count += 1;                    
-                }
-                if(initialGrid[i][j+1] == 1 && j!=col -1 ){
-                    count += 1;
-                }
-
-                // Adjacent rows
-                if(initialGrid[i - 1][j] == 1 && i!=0){
-                    count += 1; 
-                }
-                if(initialGrid[i+1][j] == 1 && i!= row-1){
-                    count += 1;
-                }
-
-                // Checking diagonals
-                if((initialGrid[i+1][j+1] == 1) && (i!=row-1 && j!= col-1)){
-                    count += 1;
-                }
-                if((initialGrid[i-1][j-1] == 1) && (i!=0 && j!=0)){
-                    count += 1;
-                }
-
-                // Alternate diagonal
-                if((initialGrid[i+1][j-1] == 1) && (i!= row-1 && j!=0)){
-                    count += 1;
-                }
-
-                if((initialGrid[i-1][j+1] == 1) && (i!=0 && j!=col-1)){
-                    count += 1;
-                }
-
-                if(count == 3){
-                    grid[i][j] = 1;
-                }
+        for(int j=0;j<col;j++){
+            neighbourCount = countNeighbourCell(initialGrid, i, j);
+            if((initialGrid[i][j] == 1) && (neighbourCount == 2 || neighbourCount == 3)){
+                grid[i][j] = 1;
             }
-            if(initialGrid[i][j] == 1){
-                // Adjacent Columns
-                if(initialGrid[i][j-1] == 1 && j!=0){
-                    count += 1;                    
-                }
-                if(initialGrid[i][j+1] == 1 && j!=col -1 ){
-                    count += 1;
-                }
-
-                // Adjacent rows
-                if(initialGrid[i - 1][j] == 1 && i!=0){
-                    count += 1; 
-                }
-                if(initialGrid[i+1][j] == 1 && i!= row-1){
-                    count += 1;
-                }
-
-                // Checking diagonals
-                if((initialGrid[i+1][j+1] == 1) && (i!=row-1 && j!= col-1)){
-                    count += 1;
-                }
-                if((initialGrid[i-1][j-1] == 1) && (i!=0 && j!=0)){
-                    count += 1;
-                }
-
-                // Alternate diagonal
-                if((initialGrid[i+1][j-1] == 1) && (i!= row-1 && j!=0)){
-                    count += 1;
-                }
-
-                if((initialGrid[i-1][j+1] == 1) && (i!=0 && j!=col-1)){
-                    count += 1;
-                }
-
-                if(count == 2 || count == 3){
-                    grid[i][j] = 1;
-                }else{
-                    grid[i][j] = 0;
-                }
+            else if((initialGrid[i][j] == 0) && (neighbourCount == 3)){
+                grid[i][j] = 1;
+            }
+            else{
+                grid[i][j] = 0;
             }
         }
     }
-
     cout<< "Final output is \n";
     // Final Output
     for(int i=0; i<row; i++){
